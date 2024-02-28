71449cedf78639970c58b31983eeec8813ed307d
Parviz_8901 <parvizakmalov39@gmail.com>
2024-02-29 01:40:53 +0500

    js 3

---
+++ main/main.js
@@ -0,0 +1,40 @@
+console.log("                                         1-vazifa");
+
+let a = prompt("Haftada nechchi kun bor?")
+let b = prompt("1 kunimiz nechchi soat?")
+
+let d = parseFloat(a)
+let v = parseFloat(b)
+
+console.log(
+   `1 haftada ${v * d} soat bor`
+);
+
+console.log("                                         2-vazifa");
+
+let s = prompt("2-misol: 2*2=?")
+let n = prompt("5 yoki 6")
+let o = prompt("Nechchida siz?")
+
+let i = parseFloat(s)
+let r = parseFloat(n)
+let f = parseFloat(o)
+
+console.log(
+    i ** r / f 
+);
+
+
+console.log("                                         3-vazifa");
+
+let t = prompt("3-misol: Hozirgi yil?")
+let m = prompt("1112-100")
+let q = prompt("3 yoki 4")
+
+let x = parseFloat(t)
+let g = parseFloat(m)
+let j = parseFloat(q)
+
+console.log(
+    x / m * j 
+);
